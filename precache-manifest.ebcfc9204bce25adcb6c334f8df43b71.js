23954b4d0d0664e0abb506bc06f6626ec73a6f81
David NoMa <davidnm@gmail.com>
2020-07-13 15:19:47 +0100

    Updates

---
+++ precache-manifest.ebcfc9204bce25adcb6c334f8df43b71.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc2c513c42d76018cc6d63b88b11c16",
+    "url": "/visual-design/index.html"
+  },
+  {
+    "revision": "6f8a3e4d77a8196d7fce",
+    "url": "/visual-design/static/css/main.da434465.chunk.css"
+  },
+  {
+    "revision": "1032e0182d7a51e2ee48",
+    "url": "/visual-design/static/js/2.3fb977df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/visual-design/static/js/2.3fb977df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8a3e4d77a8196d7fce",
+    "url": "/visual-design/static/js/main.dfa19c97.chunk.js"
+  },
+  {
+    "revision": "1db9bc59c040644eeabe",
+    "url": "/visual-design/static/js/runtime-main.8d2635e1.js"
+  },
+  {
+    "revision": "96336f949a06960928dac01ef4cbb7e7",
+    "url": "/visual-design/static/media/design1_colourBalance.96336f94.jpg"
+  },
+  {
+    "revision": "f8da96065b800a0de3aed5a45f2637a9",
+    "url": "/visual-design/static/media/design1_levels.f8da9606.jpg"
+  },
+  {
+    "revision": "2e1677636b5b5df437d6cda96bc06585",
+    "url": "/visual-design/static/media/design1_no_edit.2e167763.jpg"
+  },
+  {
+    "revision": "93671fd3687669bbd0ae710ea07ca16c",
+    "url": "/visual-design/static/media/design1_no_fold.93671fd3.jpg"
+  },
+  {
+    "revision": "2dd06984ac0f3c5726fba4426761a242",
+    "url": "/visual-design/static/media/design1_sky.2dd06984.jpg"
+  }
+]);
